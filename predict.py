10127d2a4ce4a826e31b51c1165db9744803b313
hbc123ht <hbc123ht@gmail.com>
2021-10-19 13:01:49 +0000

    nvm

--- predict.py
+++ predict.py
@@ -12,7 +12,7 @@ if __name__ == '__main__':
     #initiate argparse
     parser = argparse.ArgumentParser()
     parser.add_argument("--load_checkpoint_dir", dest="LOAD_CHECKPOINT_DIR", type=str, required=True, help="Path to checkpoint")
-    # parser.add_argument("--load_pdf_dir", dest="LOAD_PDF_DIR", type=str, required=True, help="Path to pdf")
+    parser.add_argument("--MAX_LEN", dest="MAX_LEN", type=int, default = 90, help="This should be strictly greater than 20")
     args = parser.parse_args()
 
     # load tag2idx
@@ -23,17 +23,13 @@ if __name__ == '__main__':
     # initate model
     tokenizer = PhobertTokenizer.from_pretrained("vinai/phobert-base", do_lower_case =False)   # initiate config
     config = BertConfig.from_pretrained("vinai/phobert-base", num_labels = len(tag2idx))
+
     # initial model
     model = BERTClass(config)
     model.load_state_dict(torch.load(os.path.join(args.LOAD_CHECKPOINT_DIR, 'weights.pt'),  map_location=torch.device('cpu')))
 
     # read data
-    data = pd.read_json('test.json', lines=True)
-
-
-    for _ in range(19):
-        example = data['content'][_]
-        print(example)
-        print(make_prediction(example, idx2tag, model = model, tokenizer = tokenizer))
-        print("------------------------------------------------------------------------------------------------")
+    example = "Nguyễn Văn A Ngày sinh: 01/01/1990 Mail: abc@gmail.com 123 Hoàn Kiếm, Hà Nội  Trường Đại học Bách khoa Hà Nội"
+    print(example)
+    print(make_prediction(example, idx2tag, max_len = args.MAX_LEN, model = model, tokenizer = tokenizer))
 
