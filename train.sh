933b37d2146d49d4da639b278e12b6416504b02f
hbc123ht <hbc123ht@gmail.com>
2021-10-27 13:03:06 +0700

    best config so far

--- train.sh
+++ train.sh
@@ -1,9 +1,9 @@
-python3 train.py --max_len 90 \
-                --data_dir 'Vietnamese Entity Recognition in Resumes.json' \
-                --save_checkpoint_dir 'checkpoint_1' \
+python3 train.py --max_len 60 \
+                --data_dir 'train.json' \
+                --save_checkpoint_dir 'checkpoint' \
                 --save_checkpoint_fre 1 \
-                --overlap_size 60 \
-                --lr 3e-5 \
-                --batch_num 10 \
+                --overlap_size 20 \
+                --lr 5e-5 \
+                --batch_num 40 \
                 --num_epoch 25 \
                 --pretrained_model "vinai/phobert-base"
